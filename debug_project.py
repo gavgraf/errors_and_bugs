70c4dd8412b3effc7a801bf9148c129a3ee4e4f7
Артем GAV <robockop_12@mail.ru>
2025-02-10 13:15:42 +0300

    debug

--- debug_project.py
+++ debug_project.py
@@ -2,20 +2,21 @@ def get_full_name(data):
     """
     Возвращает строку, содержащую имя и фамилию пользователя.
     """
-    return data['first_name'] + ' ' + data['age']
-    
-    
+    return data['first_name'] + ' ' + data['last_name']
+
+
 def has_full_access(data):
     """
     Возвращает True, если возраст пользователя не меньше 18 лет.
     """
     return data['age'] < 18
 
+
 user_info = {'first_name': 'Алёна', 'last_name': 'Петрова', 'age': 25}
 full_name = get_full_name(user_info)
 print(f'Здравствуйте, {full_name}')
 
-if has_full_access(user_info)
-    print('Вам есть 18 лет и у вас есть доступ ко всем возможностям сайта.')
-else
+if has_full_access(user_info):
     print('Вам нет 18 лет, ваш доступ ограничен.')
+else:
+    print('Вам есть 18 лет и у вас есть доступ ко всем возможностям сайта.')
